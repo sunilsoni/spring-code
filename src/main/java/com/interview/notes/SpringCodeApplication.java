58e1fbbefb87a150a5f10e9bfb18436c52bc345f
sunilsoni <sunilsoni@live.in>
2024-06-26 20:30:00 +0530

    intial commit

---
+++ src/main/java/com/interview/notes/SpringCodeApplication.java
@@ -0,0 +1,14 @@
+package com.interview.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCodeApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/interview/notes/SpringCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
